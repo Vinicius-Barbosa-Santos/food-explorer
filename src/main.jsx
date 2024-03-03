93bd84a6f7c45c8ddfcad06b078f01bef820620c
Vinicius-Barbosa-Santos <viniciusfoxsantos@gmail.com>
2024-03-02 22:32:37 -0300

    feat: adjustment global style :gear:

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { App } from "./App.jsx";
+import { Global } from "./styles/global.js";
+import { defaultTheme } from "./styles/theme/default.js";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <Global theme={defaultTheme}>
+      <App />
+    </Global>
   </React.StrictMode>
 );
--- src/styles/global.js
+++ src/styles/global.js
@@ -1,5 +1,34 @@
-import { createGlobalStyle } from "styled-components";
+import { createGlobalStyle } from 'styled-components'
 
 export const Global = createGlobalStyle`
-    
+  * {
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+  }
+  
+  :focus {
+    outline: 0;
+    box-shadow: 0 0 0 2px ${(props) => props.theme['light-100']};
+  }
+
+  body {
+    background-color: ${({ theme }) => theme['dark-400']};
+    color: ${({ theme }) => theme['light-100']};
+    -webkit-font-smoothing: antialiased;
+  }
+
+  body, input, textarea, button {
+    font: 400 1rem 'Roboto', sans-serif
+  }
+
+  button:hover, a:hover {
+    filter: brightness(0.9);
+  }
+
+  @media(max-width: 768px) {
+        html {
+            font-size: 87.5%;
+        }
+    }
 `
