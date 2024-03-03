8b352a8522a4015bb68fc87c9076dcd4d4ab4463
Vinicius-Barbosa-Santos <viniciusfoxsantos@gmail.com>
2024-03-02 22:24:37 -0300

    feat: default themes :gear:

---
+++ src/styles/theme/default.js
@@ -0,0 +1,37 @@
+export const defaultTheme = {
+    white: '#fff',
+
+    'light-100': '#FFFFFF',
+    'light-200': '#FFFAF1',
+    'light-300': '#E1E1E6',
+    'light-400': '#C4C4CC',
+    'light-500': '#7C7C8A',
+    'light-600': '#76797B',
+    'light-700': '#4D585E',
+  
+    'dark-100': '#000405',
+    'dark-200': '#00070A',
+    'dark-300': '#000204',
+    'dark-400': '#8D8D99',
+    'dark-500': '#000C12',
+    'dark-600': '#00111A',
+    'dark-700': '#001119',
+    'dark-800': '#0D161B',
+    'dark-900': '#0D1D25',
+    'dark-1000': '#192227',
+
+    // Lembrar de Adicionar os Grad
+  
+    'tints-100': '#750310',
+    'tints-200': '#92000E',
+    'tints-300': '#AB222E',
+    'tints-400': '#AB4D55',
+  
+    'carrot-100': '#FBA94C',
+
+    'mint-100': '#04D361',
+
+
+    'cake-200': '#82F3FF',
+    'cake-100': '#065E7C',
+  }
